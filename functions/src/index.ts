d434d3e8afbb38c18bc6b61173a59d54a4e8af1a
Mohamed-Halat-iseehear <halatmohamed@hotmail.com>
2021-04-10 16:37:14 -0400

    add lodash and scss

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,8 @@ import * as functions from "firebase-functions";
 import * as express from "express";
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 exports.app = functions.https.onRequest(app);
