749db489084dc95caa8459150065a9062035d960
Mohamed-Halat-iseehear <halatmohamed@hotmail.com>
2021-04-18 17:46:45 -0400

    fix nav footer

--- src/components/app-header/app-header.vue
+++ src/components/app-header/app-header.vue
@@ -1,16 +1,7 @@
 <template>
   <header class="app-header">
-    <nav-bar :routes="routes">
-    </nav-bar>
-    <footer
-      class="nav-footer"
-      style="
-        height: 2px;
-        width: 100%;
-        background-color: #fcb934;
-        position: fixed;
-      "
-    ></footer>
+    <nav-bar :routes="routes"> </nav-bar>
+    <footer class="nav-footer"></footer>
   </header>
 </template>
 <script>
@@ -33,5 +24,10 @@ header.app-header {
   top: 0;
   width: 100%;
   background-color: white;
+}
+.nav-footer {
+  height: 2px;
+  width: 100%;
+  background-color: #fcb934;
 }
 </style>
