dc2efb020e6a9c4bfd8a1bcb387c3b7989523e62
Mohamed-Halat-iseehear <halatmohamed@hotmail.com>
2021-04-18 19:04:46 -0400

    use bootstrap cards

--- src/views/Covid.vue
+++ src/views/Covid.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <p-datatable :value="countryData" :paginator="true" :rows="20">
-      <p-column field="country" header="Country" sortable></p-column>
-      <p-column field="cases" header="Cases" sortable></p-column>
-      <p-column field="deaths" header="Deaths" sortable></p-column>
-      <p-column field="active" header="Active Cases" sortable></p-column>
-    </p-datatable>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Country Data</h5>
+        <p class="card-text">
+          <p-datatable :value="countryData" :paginator="true" :rows="20">
+            <p-column field="country" header="Country" sortable></p-column>
+            <p-column field="cases" header="Cases" sortable></p-column>
+            <p-column field="deaths" header="Deaths" sortable></p-column>
+            <p-column field="active" header="Active Cases" sortable></p-column>
+          </p-datatable>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
